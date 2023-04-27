7b9abd55361c878dc6016fd5af826b393142ebe8
huixin <huixin.22@ichat.sp.edu.sg>
2023-04-27 15:56:19 +0800

    added bmi calculator

---
+++ bmi.py
@@ -0,0 +1,6 @@
+def calculate_bmi (height,weight):
+    print("Height = " + str(height))
+    print ("Weight = " + str(weight))
+    bmi = weight/(height*height)
+    print("Bmi = "+ str(bmi))
+calculate_bmi(weight = 57, height = 1.73)
