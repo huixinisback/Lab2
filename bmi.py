ec297ce619e8919dc92398ce29fec12d7b770778
huixin <huixin.22@ichat.sp.edu.sg>
2023-05-04 15:59:54 +0800

    changes made to lab 2 and 3

--- bmi.py
+++ bmi.py
@@ -4,15 +4,17 @@ def calculate_bmi (height,weight):
     print ("Weight = " + str(weight))
     bmi = weight/(height*height)
     print("Bmi = "+ str(bmi))
-    if (bmi<19):
+    if (bmi<18.5):
         value= -1
+
     elif (bmi > 24):
         value =1
+
     else:
         value = 0
     print ("Returning Value = " + str(value))
-
-calculate_bmi(weight = 57, height = 1.73)
+    return value
+calculate_bmi(weight = 80, height = 1.73)
 
 
 
