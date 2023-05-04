f76663f7c6dcce83d52220c0f28a91c547ad1fca
huixin <huixin.22@ichat.sp.edu.sg>
2023-05-04 14:49:38 +0800

    bmi returning value

--- bmi.py
+++ bmi.py
@@ -1,6 +1,19 @@
+
 def calculate_bmi (height,weight):
     print("Height = " + str(height))
     print ("Weight = " + str(weight))
     bmi = weight/(height*height)
     print("Bmi = "+ str(bmi))
+    if (bmi<19):
+        value= -1
+    elif (bmi > 24):
+        value =1
+    else:
+        value = 0
+    print ("Returning Value = " + str(value))
+
 calculate_bmi(weight = 57, height = 1.73)
+
+
+
+
